44131439f63724992516c57e406304b612a2af5d
Victor Martins <victor.martins.1310@hotmail.com>
2025-08-30 01:40:57 +0200

    Changed the Requestmapping for better accessing

--- src/main/java/com/bootcamp/project/controller/implement/AdminController.java
+++ src/main/java/com/bootcamp/project/controller/implement/AdminController.java
@@ -1,5 +1,6 @@
 package com.bootcamp.project.controller.implement;
 
+import com.bootcamp.project.model.ProductType;
 import com.bootcamp.project.model.Role;
 import com.bootcamp.project.model.User;
 import com.bootcamp.project.service.UserService;
@@ -7,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -19,26 +21,36 @@ import java.util.UUID;
  */
 @RequiredArgsConstructor
 @RestController
-@RequestMapping(name = "admin", value = "api/admin")
+@RequestMapping()
 public class AdminController {
     private final UserService userService;
+    private final String admin = "api/admin";
+
+
+
+    @GetMapping("/types")
+    public List<String> getTypeValues() {
+        return Arrays.stream(ProductType.values())
+                .map(Enum::name)
+                .toList();
+    }
 
     /**
      * Save a new role
      * @param role
      * @return
      */
-    @PostMapping(value = "/roles")
+    @PostMapping(value = admin + "/roles")
     @ResponseStatus(HttpStatus.CREATED)
     public Role saveRole(@RequestBody String role) { return userService.addRole(role); }
-    @GetMapping(value ="/users")
+    @GetMapping(value = admin + "/users")
     public List<User> showAllUsers(){ return userService.showUsers(); }
     @GetMapping(value = "/users/{id}")
     @ResponseStatus(HttpStatus.OK)
     public User showDetails(@PathVariable(name = "id") UUID id){
         return userService.findByUserID(id);
     }
-    @DeleteMapping(value = "/users/{id}")
+    @DeleteMapping(value = admin + "/users/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteUserByID(@PathVariable(name = "id") UUID id){
         userService.deleteUserByID(id);
