385a4cdfb9a1a4f6f5f746e03efcce5fa102f1f0
Victor Martins <victor.martins.1310@hotmail.com>
2026-02-12 02:45:30 +0100

    Renamed Todo List, changed to abstract class

---
+++ src/main/java/com/bootcamp/project/model/TodoItem.java
@@ -0,0 +1,30 @@
+package com.bootcamp.project.model;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@Entity
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Inheritance(strategy = InheritanceType.JOINED)
+public abstract class TodoItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long todoID;
+    private Date creationDate = new Date();
+    /** True means Done, False to be Do */
+    private Boolean completed = false;
+
+    protected String type = this.getClass().getSimpleName();
+
+    @NotNull
+    @ManyToOne(optional = false)
+    private User user;
+}
